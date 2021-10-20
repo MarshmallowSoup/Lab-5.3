1fd6bd8057781e7abb676cd13d3a067a01dac17d
MarshmallowSoup <lysetskyyv@gmail.com>
2021-10-21 00:30:12 +0300

    Algorythms and Programming Lab 5.3

---
+++ Lab_5-3/Lab_5-3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double f(const double x);
+int main()
+{
+	double tp, tk, z, x;
+	int n;
+	cout << "tp = "; cin >> tp;
+	cout << "tk = "; cin >> tk;
+	cout << "n = "; cin >> n;
+	double t = tp;
+	cout << fixed;
+		cout << "-------------------------------------------------" << endl;
+		cout << "|" << setw(5) << "t" << " |"
+			<< setw(10) << "z" 
+			<< endl;
+		cout << "-------------------------------------------------" << endl;
+	while (t <= tk)
+	{		
+	    z = f(t * t) + 2 * f(2 * t + 1);
+		cout << t << " " << z << endl;
+		t += n;
+	}
+	return 0;
+}
+double f(const double x)
+{
+	if (abs(x) >= 1)
+		return (sin(x) + 1) / (sin(x) + cos(x));
+	else
+	{
+		double S;
+		int i = 0;
+		double a = pow(x,3);
+		S = a;
+		do
+		{
+			i++;
+			double R = x * x / (2. * i * (1 + 2. * i));
+			a *= R;
+			S += a;
+		} while (i < 8);
+		return S;
+	}
+}
---
+++ UnitTest-Lab_5-3/UnitTest-Lab_5-3.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Lab_5-3\Lab_5-3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab53
+{
+	TEST_CLASS(UnitTestLab53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double k = f(0);
+			Assert::AreEqual(0., k);
+		}
+	};
+}
